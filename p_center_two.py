d74595214c290a55ff4ededc58862f7fe4fe9fb6
DanteDeLordran <lordran@lordran.dev>
2023-11-20 16:27:57 -0600

    Heuristic partially finished

--- p_center_two.py
+++ p_center_two.py
@@ -2,23 +2,23 @@ import numpy as np
 
 def p_center_heuristic(distance_matrix, p, n_clients, n_facilities):
     selected_facilities = []
-    assignment = [0]*n_clients
-
-    print("Initial Distance Matrix:")
-    print(distance_matrix)
+    not_selected_facilities = list(range(n_facilities))  # Initially, all facilities are not selected
+    assignment = [-1]*n_clients
 
     for iteration in range(p):
         max_distance = -1
         for i in range(n_clients):
-            for j in range(n_facilities):
-                if j not in selected_facilities and distance_matrix[i][j] > max_distance:
+            for j in not_selected_facilities:
+                if distance_matrix[i][j] > max_distance:
                     max_distance = distance_matrix[i][j]
                     max_j = j
 
         selected_facilities.append(max_j)
+        not_selected_facilities.remove(max_j)
 
         print(f"\nIteration {iteration + 1}:")
         print(f"Selected Facilities: {selected_facilities}")
+        print(f"Not Selected Facilities: {not_selected_facilities}")
 
         # Print distances for each client to the selected facilities in this iteration
         distances_to_selected = distance_matrix[:, selected_facilities]
@@ -31,21 +31,23 @@ def p_center_heuristic(distance_matrix, p, n_clients, n_facilities):
                 if distance_matrix[i][j] < min_distance:
                     min_distance = distance_matrix[i][j]
                     min_j = j
-            assignment[i] = min_j
+            assignment[i] = (i, min_j)
 
         print(f"Client Assignment: {assignment}")
 
     final_distance = max(np.min(distance_matrix[:, selected_facilities], axis=1))
 
-    return selected_facilities, assignment, final_distance
+    return selected_facilities, not_selected_facilities, assignment, final_distance
 
 distance_matrix = np.loadtxt('./sample10/distance_matrix_0.csv', delimiter=',')
-p = 4
-n_clients = 10
+p = 3
+n_clients = 5
 n_facilities = 5
-selected_facilities, assignment, final_distance = p_center_heuristic(distance_matrix, p, n_clients, n_facilities)
+selected_facilities, not_selected_facilities, assignment, final_distance = p_center_heuristic(distance_matrix, p, n_clients, n_facilities)
 
-print(f"\nNumber of possible facilities: {n_facilities}, p: {p}")
+print("\n\n-------Final report--------\n")
+print(f"Number of possible facilities: {n_facilities}, p: {p}")
 print(f"Selected Facilities: {selected_facilities}")
+print(f"Not selected Facilities: {not_selected_facilities}")
 print(f"Client Assignment: {assignment}")
 print(f"Final Maximum Distance: {final_distance}")
